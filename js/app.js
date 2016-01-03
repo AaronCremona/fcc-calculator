625089b7e73d5781b1ca7c036313f4fb701a6464
Aaron Cremona <dubbacremona@gmail.com>
2016-01-02 21:05:24 -0500

    added pseudocode and calc btn generic event handler

---
+++ js/app.js
@@ -0,0 +1,34 @@
+
+
+// store most resent result
+// select operator
+// operator click triggers display update
+
+//clear function 
+	// take stored value and reset to zero
+	// set the display to zero
+
+
+// add function
+
+// subtract function
+
+// multiply function
+
+// divide function
+
+// equals function
+
+// display function
+
+// calculator = function() {
+// 	var currentValue = 0;
+
+// 	this.updateDisplay = function() {
+
+// 	}
+// };
+
+$('.calc-btn').on('click', function(){
+	console.log($(this).attr('id'));
+});
