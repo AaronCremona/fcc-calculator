b53b969e2fcd12f74f56e0a291a3280d4fac44e4
Aaron Cremona <dubbacremona@gmail.com>
2016-01-03 17:33:07 -0500

    added switch statement to click handler to differentiate button actions

--- js/app.js
+++ js/app.js
@@ -55,7 +55,21 @@ function btnListener(e){
 		var clickedBtnId = e.target.id,
 			clickedBtnClass = e.target.classList;
 
-		c.appendValue(clickedBtnId);	
+		switch(clickedBtnId) {
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+			case '.':
+				c.appendValue(clickedBtnId);
+				break;
+			}	
 		}
 
 	e.stopPropagation();
