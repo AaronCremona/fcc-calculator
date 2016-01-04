e02c0ceb6dc46265d9574a92a7d215beb10de2da
Aaron Cremona <dubbacremona@gmail.com>
2016-01-03 19:51:59 -0500

    removed pseudocode

--- js/app.js
+++ js/app.js
@@ -1,32 +1,3 @@
-// store most resent result
-// select operator
-// operator click triggers display update
-
-//clear function 
-	// take stored value and reset to zero
-	// set the display to zero
-
-
-// add function
-
-// subtract function
-
-// multiply function
-
-// divide function
-
-// equals function
-
-// display function
-
-// calculator = function() {
-// 	var currentValue = 0;
-
-// 	this.updateDisplay = function() {
-
-// 	}
-// };
-
 function Calc() {
 	var display = "0",
 		currentValue = 0,
